c2a22db57a2b95df7b4aaf296357fdbb59208461
Emre <ey1112211@gmail.com>
2022-07-31 00:42:32 +0300

    create checkGoogleMock & checkIOSMock & isLastDigitOdd functions

---
+++ app/Helpers/PurchaseHelper.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App\Helpers;
+
+class PurchaseHelper
+{
+    public static function checkGoogleMock(string $hashedReceiptId) : array
+    {
+        return self::isLastDigitOdd($hashedReceiptId);
+    }
+
+    public static function checkIOSMock(string $hashedReceiptId) : array
+    {
+        return self::isLastDigitOdd($hashedReceiptId);
+    }
+
+    public static function isLastDigitOdd(string $hashedReceiptId) : array
+    {
+        $lastChar = substr($hashedReceiptId, -1);
+        $isOdd = (is_numeric($lastChar) and (!$lastChar % 2 == 0));
+
+        $responseInf = [
+            'status' => $isOdd
+        ];
+
+        if ($isOdd) {
+
+            $date = new \DateTime('+1 month', new \DateTimeZone('GMT-6'));
+
+            array_push(
+                $responseInf,
+                [
+                    'expire-date' =>$date->format('Y-m-d H:i:sP')
+                ]
+            );
+        }
+
+        return $responseInf;
+    }
+
+}
