7cdd695d06b50d0cc620e9ca317843c359ad20fb
Emre <ey1112211@gmail.com>
2022-07-30 05:09:01 +0300

    migration: create devices table

---
+++ database/migrations/2022_07_30_014516_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('device_uuid');
+            $table->integer('app_id');
+            $table->enum('language', ['en', 'tr']);
+            $table->enum('os', ['android', 'ios']);
+            $table->timestamps();
+
+            $table->index('device_uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
