3b96b3e63fae540e6225b269722bc4ea92b93a7b
Emre <ey1112211@gmail.com>
2022-07-30 20:59:53 +0300

    use upsert method instead of updateOrCreate method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,16 +9,22 @@ class ApiController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        return Device::updateOrCreate(
+        Device::upsert(
             [
-                'device_uuid' => $request->device_uuid
+                    'device_uuid'   => $request->device_uuid,
+                    'app_id'        => $request->app_id,
+                    'language'      => $request->language,
+                    'os'            => $request->os,
+                    'client_token'  => Str::uuid()->toString()
             ],
-            [
-                'app_id'        => $request->app_id,
-                'language'      => $request->language,
-                'os'            => $request->os,
-                'client_token'  => Str::uuid()->toString()
-            ]
+            ['device_uuid'],
+            ['app_id','language','os'] // don't update client_token if there is a record with this device_uuid...
         );
+
+        // if we used updateOrCreate method for upsert we wouldn't use this where query because updateOrCreate gives the row...
+        // But upsert method returns boolean and we have to use it because we don't want update client_token...
+
+        return Device::where('device_uuid', $request->device_uuid)->first();
+
     }
 }
