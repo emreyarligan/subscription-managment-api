b9ae300f5f2565c3340fc6985603c7c6635f7543
Emre <ey1112211@gmail.com>
2022-07-31 01:31:31 +0300

    create purchase request

---
+++ app/Http/Requests/PurchaseRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class PurchaseRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'receiptId'     => 'required|integer',
+            'clientToken'   => 'required|uuid',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
