8e377c146eae4aa680f0fd0e1c790564245b0e45
Emre <ey1112211@gmail.com>
2022-07-31 03:34:15 +0300

    create Subscription model

---
+++ app/Models/Subscription.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['client_token','receipt_id','expire_date','api_result'];
+}
