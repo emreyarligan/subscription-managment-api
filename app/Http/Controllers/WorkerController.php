b08a49cceec1abcdb5ea22ae5e432e393ad051a6
Emre <ey1112211@gmail.com>
2022-08-01 03:19:00 +0300

    create prepareQueue worker function

---
+++ app/Http/Controllers/WorkerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Subscriptions;
+use App\Jobs\SubscriptionPolling;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class WorkerController extends Controller
+{
+    public function prepareQueue()
+    {
+        $subscriptions = Subscriptions::where(
+            'status', true,
+        )->where(
+            'expire_date', '<=', date('Y-m-d H:i:s',time())
+        )->get()->toArray();
+
+        if ($subscriptions) {
+            foreach ($subscriptions as $subscription) {
+                SubscriptionPolling::dispatch($subscription['client_token'])->onQueue('subscriptionPolling');
+            }
+
+            throw new HttpResponseException(response()->json([
+                'success'   => true,
+                'message'   => count($subscriptions).' data added to the subscriptionPolling quene',
+            ]));
+        }
+
+
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'There is no active and expired subscription',
+        ]));
+        
+    }
+}
