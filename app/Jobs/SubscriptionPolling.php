6fb87415fb79763992be9d3e11f9fcd7d4851e29
Emre <ey1112211@gmail.com>
2022-08-01 03:18:26 +0300

    create SubscriptionPolling job

---
+++ app/Jobs/SubscriptionPolling.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Subscriptions;
+use App\Models\PurchaseHistory;
+use App\Helpers\PurchaseHelper;
+
+
+class SubscriptionPolling implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $client_token;
+
+    public function __construct($client_token)
+    {
+        $this->client_token = $client_token;
+    }
+
+
+    public function handle()
+    {
+        $lastReceiptId = PurchaseHistory::where(['client_token' => $this->client_token])->latest()->first()->receipt_id;
+        $checkRateLimits = PurchaseHelper::checkLastTwoDigitsDivisibleBySix($lastReceiptId);
+
+        if ($checkRateLimits) {
+            $date = new \DateTime('+1 month', new \DateTimeZone('GMT-6'));
+            Subscriptions::where('client_token',$this->client_token)->update(['expire_date' => $date->format('Y-m-d H:i:s')]);
+        }
+
+    }
+}
